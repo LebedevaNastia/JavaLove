690a44af5b90fbf13ffadb4a06823dc6f12bfbfe
Nastia <nastia.lebedeva@gmail.com>
2016-12-14 00:34:00 +0300

    Tipe

---
+++ Tipe/src/Tipe.java
@@ -0,0 +1,24 @@
+
+public class Tipe {
+    public static void main (String[] args){
+        int i = 15;
+        double z = 6.7;
+        byte x = 3;
+        char r = 'f';
+
+        z = 4.3;
+        i = 7;
+        System.out.println("z = " + z);
+        System.out.println("i = " + i);
+
+        int z1 = (int) z;
+        System.out.println("z1 = " + z1);
+        double i1 = 1;
+        System.out.println("i1 = " + i1);
+
+
+
+
+
+    }
+}
